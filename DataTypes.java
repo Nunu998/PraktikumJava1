04ea214960aefe7bf7f07d6e7ca7d67598c6cbfd
Nunu998 <84434671+Nunu998@users.noreply.github.com>
2021-05-19 15:09:08 +0700

    Deklarasi Variabel dan Tipe Data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[]args){
+	 
+	 String namaDepan = "Ibnu";
+	 String namaTengah = "Azmi";
+	 String namaBelakang = "Nazira";
+	 int usia = 20;
+	 int targetTahunKuliah = 4;
+	 double ipk = 3.69;
+	 char nilaiAbjad = 'A';
+	 boolean tampan = true;
+ }
+}
